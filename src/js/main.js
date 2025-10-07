a590dd494278df87150ca0d6b3bc0e4f616cfe05
Jales Nunes <jales.nunes@hotmail.com>
2025-10-07 11:11:39 -0600

    aded link css tag to index

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 // import '../../public/assets/bootstrap/bootstrap.min.css';
 // import '../../public/assets/bootstrap/bootstrap.bundle.min.js';
-import '../css/style.css';
+// import '../css/style.css';
